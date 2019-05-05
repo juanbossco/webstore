c49d72609c146f7af1f2e1475dd79ad83287cd1a
jbossco <juanbossco@gmail.com>
2019-05-05 11:27:29 -0700

    added Events project

---
+++ codebase/Events/Events.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Webstore.Events
+{
+    public class ProductCreated
+    {
+        public int ProductId { get; set; }
+    }
+
+    public class OrderPlaced
+    {
+        public long OrderId { get; set; }
+        public string SessionId { get; set; }
+        public string OrderNumber { get; set; }
+    }
+
+    public class OrderShipped
+    {
+        public long OrderId { get; set; }
+        public string OrderNumber { get; set; }
+    }
+
+    public class OrderCancelled
+    {
+        public long OrderId { get; set; }
+        public string OrderNumber { get; set; }
+    }
+}
